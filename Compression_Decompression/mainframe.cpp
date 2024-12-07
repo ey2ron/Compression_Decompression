ae6da2b853fcee6aed4305a659600bd8f094ba46
ey2ron <eyrownklayd24@gmail.com>
2024-12-08 01:32:28 +0800

    tapos na yung dropbox for compression pati decompression
    lagay nalang yung implementation na galing kay axel
    
    tapos dagdag nalang tayo feature na malalagay sa program yung file na ginawa tapos dragabble
    
    after non linis nalang

--- Compression_Decompression/mainframe.cpp
+++ Compression_Decompression/mainframe.cpp
@@ -1,12 +1,10 @@
 #include "mainframe.h"
 
-wxPanel* secondpanel;
-wxPanel* creatingtextpanel;
+wxBEGIN_EVENT_TABLE(mainframe, wxFrame)
+EVT_BUTTON(wxID_ANY, mainframe::Creatingfile)
+EVT_BUTTON(wxID_ANY, mainframe::DoneButtonF)
+wxEND_EVENT_TABLE()
 
-wxButton* CreateFileButton;
-wxButton* DoneButton;
-
-wxTextCtrl* textcontents;
 mainframe::mainframe(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) {
     SPanel();
 }
@@ -14,25 +12,63 @@ mainframe::mainframe(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) 
 void mainframe::SPanel() {
     secondpanel = new wxPanel(this);
 
-    CreateFileButton = new wxButton(secondpanel, wxID_ANY, "Create Text File", wxPoint(30, 30), wxSize(150, 50));
+    wxStaticText* panelLabel = new wxStaticText(secondpanel, wxID_ANY, "File Management Panel",
+        wxPoint(30, 10), wxDefaultSize, wxALIGN_LEFT);
+
+    CreateFileButton = new wxButton(secondpanel, wxID_ANY, "Create Text File", wxPoint(30, 50), wxSize(150, 50));
     CreateFileButton->Bind(wxEVT_BUTTON, &mainframe::Creatingfile, this);
+
+    // Compress Drop Box
+    wxStaticText* compressLabel = new wxStaticText(secondpanel, wxID_ANY, "Drag files here to Compress:", wxPoint(200, 120), wxDefaultSize, wxALIGN_LEFT);
+    compressBox = new wxListBox(secondpanel, wxID_ANY, wxPoint(200, 140), wxSize(300, 200));
+    compressBox->SetDropTarget(new FileDropTarget(compressBox, "Compress"));
+
+    // Decompress Drop Box
+    wxStaticText* decompressLabel = new wxStaticText(secondpanel, wxID_ANY, "Drag files here to Decompress:", wxPoint(550, 120), wxDefaultSize, wxALIGN_LEFT);
+    decompressBox = new wxListBox(secondpanel, wxID_ANY, wxPoint(550, 140), wxSize(300, 200));
+    decompressBox->SetDropTarget(new FileDropTarget(decompressBox, "Decompress"));
+
+   
 }
 
 void mainframe::Creatingfile(wxCommandEvent& event) {
     secondpanel->Hide();
 
-    creatingtextpanel = new wxPanel(this, wxID_ANY, wxPoint(1,1), wxSize(1280, 720));
+    creatingtextpanel = new wxPanel(this, wxID_ANY, wxPoint(1,1), wxSize(1280,720));
+    wxBoxSizer* textPanelSizer = new wxBoxSizer(wxVERTICAL);
 
-    textcontents = new wxTextCtrl(creatingtextpanel, wxID_ANY, "", wxPoint(50, 50), wxSize(960, 500), wxTE_MULTILINE);
 
-    DoneButton = new wxButton(creatingtextpanel, wxID_ANY, "Done", wxPoint(1100, 660), wxSize(150, 50));
+    wxStaticText* textEditorLabel = new wxStaticText(creatingtextpanel, wxID_ANY, "Enter your text below:",
+        wxDefaultPosition, wxDefaultSize, wxALIGN_LEFT);
+
+    textcontents = new wxTextCtrl(creatingtextpanel, wxID_ANY, "", wxPoint (50,50), wxSize(960, 500), wxTE_MULTILINE);
+    DoneButton = new wxButton(creatingtextpanel, wxID_ANY, "Done", wxPoint(1100,660), wxSize(150, 50));
     DoneButton->Bind(wxEVT_BUTTON, &mainframe::DoneButtonF, this);
 
+    textPanelSizer->Add(textEditorLabel, 0, wxALL | wxLEFT, 10);
+    textPanelSizer->Add(textcontents, 1, wxEXPAND | wxALL, 10);
+    textPanelSizer->Add(DoneButton, 0, wxALL | wxALIGN_RIGHT, 10);
+
+    creatingtextpanel->SetSizer(textPanelSizer);
+    creatingtextpanel->Show();
+    Layout();
 }
 
 void mainframe::DoneButtonF(wxCommandEvent& event) {
     creatingtextpanel->Hide();
+    wxFileDialog saveFileDialog(this, _("Save Text File"), "", "",
+        "Text files (*.txt)|*.txt", wxFD_SAVE | wxFD_OVERWRITE_PROMPT);
+
+    if (saveFileDialog.ShowModal() == wxID_CANCEL) {
+        return; 
+    }
+
+    std::ofstream outFile(saveFileDialog.GetPath().ToStdString());
+    if (outFile.is_open()) {
+        outFile << textcontents->GetValue().ToStdString();
+        outFile.close();
+    }
 
     secondpanel->Show();
-
+    Layout();
 }
--- Compression_Decompression/mainframe.h
+++ Compression_Decompression/mainframe.h
@@ -1,12 +1,66 @@
-#pragma once
-#include <wx/wx.h>
-class mainframe : public wxFrame
-{
-public:
-	mainframe(const wxString& title);
-private:
-	void SPanel();
-	void Creatingfile(wxCommandEvent& evt);
-	void DoneButtonF(wxCommandEvent& evt);
-};
+    #pragma once
+    #include <wx/wx.h>
+    #include "wx/dnd.h"
+    #include "wx/filedlg.h"
+    #include "wx/listbox.h"
+    #include <fstream>
+    #include <string>
 
+    class FileDropTarget : public wxFileDropTarget {
+    public:
+        FileDropTarget(wxListBox* listBox, const std::string& operationType)
+            : m_listBox(listBox), m_operationType(operationType) {}
+
+        virtual bool OnDropFiles(wxCoord x, wxCoord y, const wxArrayString& filenames) override {
+            for (const auto& file : filenames) {
+                m_listBox->Append(file);
+                if (m_operationType == "Compress") {
+                    CompressFile(file.ToStdString());
+                }
+                else if (m_operationType == "Decompress") {
+                    DecompressFile(file.ToStdString());
+                }
+
+                int index = m_listBox->FindString(file);
+                if (index != wxNOT_FOUND) {
+                    m_listBox->Delete(index);
+                }
+            }
+            return true;
+        }
+
+    private:
+        wxListBox* m_listBox;
+        std::string m_operationType;
+
+        void CompressFile(const std::string& filePath) {
+            // Placeholder logic for file compression
+            wxLogMessage("Compressed: %s", filePath);
+        }
+
+        void DecompressFile(const std::string& filePath) {
+            // Placeholder logic for file decompression
+            wxLogMessage("Decompressed: %s", filePath);
+        }
+    };
+
+    class mainframe : public wxFrame {
+    public:
+        mainframe(const wxString& title);
+
+    private:
+        void SPanel();
+        void Creatingfile(wxCommandEvent& evt);
+        void DoneButtonF(wxCommandEvent& evt);
+
+        wxPanel* secondpanel;
+        wxPanel* creatingtextpanel;
+        wxButton* CreateFileButton;
+        wxButton* DoneButton;
+        wxTextCtrl* textcontents;
+    
+        wxListBox* compressBox;
+        wxListBox* decompressBox;
+
+        wxDECLARE_EVENT_TABLE();
+    };
